cd6d4f07e5246295c89c475908b91f357fa4dc12
Dmitry Vorobiov <2belikespring@gmail.com>
2013-07-03 18:15:30 +0300

    Update MentorspaceBuild.scala

--- project/MentorspaceBuild.scala
+++ project/MentorspaceBuild.scala
@@ -2,9 +2,9 @@ import sbt._
 import Keys._
 import play.Project._
 
-object WaterfallBuild extends Build {
+object MentorspaceBuild extends Build {
 
-   val appName         = "waterfall"
+   val appName         = "mentorspace"
    val appVersion      = "0.0.1"
 
    val postgres = "postgresql" % "postgresql" % "9.1-901.jdbc4"
